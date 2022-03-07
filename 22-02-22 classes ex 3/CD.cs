96c2bb6b878fe1a4fae858b5d67fd5c67bf52c56
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-03-07 09:21:27 +0100

    9:20 07/03/22

--- 22-02-22 classes ex 3/CD.cs
+++ 22-02-22 classes ex 3/CD.cs
@@ -8,5 +8,18 @@ namespace _22_02_22_classes_ex_3
     {
         public string Titel;
         public string Uitvoerder;
+
+        public CD()
+        {
+            Titel = "Titel";
+            Uitvoerder = "Uitvoerder";
+
+        }
+        public CD(string OpvragenTitel, string OpvragenUitvoerder)
+        {
+
+            Titel = OpvragenTitel;
+            Uitvoerder = OpvragenUitvoerder;
+        }
     }
 }
--- 22-02-22 classes ex 3/Program.cs
+++ 22-02-22 classes ex 3/Program.cs
@@ -7,11 +7,38 @@ namespace _22_02_22_classes_ex_3
     {
         static void Main(string[] args)
         {
-
             List<CD> CDLijst = new List<CD>();
+            do
+            {
+                Input(CDLijst);
+                Console.WriteLine("Wilt u nog een liedje toevoegen?? (J/N)");
+            } while (Console.ReadLine().ToUpper().Substring(0, 1) == "J");
+            output(CDLijst);
+        }
+
+        private static void output(List<CD> CDLijst)
+        {
+            Console.WriteLine($"Er zijn {CDLijst.Count} Liedjes");
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+
+        private static List<CD> Input(List<CD> CDLijst)
+        {
+            CD Data;
+            Console.Clear();
+            Data = new CD();
+
+            Console.WriteLine("Wat is de titel van het lied?");
+            Data.Titel = Console.ReadLine();
+
+            Console.WriteLine("Wie is de uitvoerder van het lied");
+            Data.Uitvoerder = Console.ReadLine();
 
 
-            Console.WriteLine("Hello World!");
+            CDLijst.Add(Data);
+            return CDLijst;
         }
     }
 }
