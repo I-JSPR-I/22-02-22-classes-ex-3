1613a4de5195348bf2366b5eeee58714fe1dc297
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-03-07 08:59:13 +0100

    Add project files.

---
+++ 22-02-22 classes ex 3/CD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22_02_22_classes_ex_3
+{
+    class CD
+    {
+        public string Titel;
+        public string Uitvoerder;
+    }
+}
---
+++ 22-02-22 classes ex 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace _22_02_22_classes_ex_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<CD> CDLijst = new List<CD>();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
